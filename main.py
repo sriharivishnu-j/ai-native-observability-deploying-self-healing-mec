665639a3c011745f1f46a348981b805287d76818
Sriharivishnu Janakiraman <j.sriharivishnu@gmail.com>
2026-01-30 22:47:14 +0800

    Add main.py

---
+++ main.py
@@ -0,0 +1,10 @@
+from fastapi import FastAPI
+from routers import observability
+import uvicorn
+
+app = FastAPI()
+
+app.include_router(observability.router)
+
+if __name__ == "__main__":
+    uvicorn.run(app, host="0.0.0.0", port=8000)
